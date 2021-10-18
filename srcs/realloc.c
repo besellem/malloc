e972c145688e98cae6fca3df22f704cfc736c15a
Benjamin Sellem <69735857+besellem@users.noreply.github.com>
2021-10-18 10:56:26 +0200

    init

--- srcs/realloc.c
+++ srcs/realloc.c
@@ -6,7 +6,7 @@
 /*   By: besellem <besellem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 10:02:49 by besellem          #+#    #+#             */
-/*   Updated: 2021/10/18 10:52:28 by besellem         ###   ########.fr       */
+/*   Updated: 2021/10/18 10:56:11 by besellem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@ static void	_init(malloc_t *p)
 {
 	ft_memset(p, 0, sizeof(malloc_t));
 	pthread_mutex_init(&p->_m, NULL);
+	pthread_mutex_lock(&p->_m);
 }
 
 static void	_end(malloc_t *p)
 {
+	pthread_mutex_unlock(&p->_m);
 	pthread_mutex_destroy(&p->_m);
 	ft_memset(p, 0, sizeof(malloc_t));
 }
